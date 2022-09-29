8792ed1e8d17d7220fa9137b55dedc7d7c51a39e
AtulKumarBisoyi <94751610+AtulKumarBisoyi@users.noreply.github.com>
2022-09-29 22:27:16 +0530

    Update d4p9.c

--- d4p9.c
+++ d4p9.c
@@ -1,17 +1,23 @@
 #include<stdio.h>
+void lcm(int,int);
 void main()
 {
-    int a,b,i,j,c=0;
+    int a,b;
     printf("Enter two number= ");
     scanf("%d%d",&a,&b);
     printf("lcm of a and b=\n");
-    for(i=1;i<=a*b;i++)
+    lcm(a,b);   
+}
+void lcm(int p,int q)
+{
+    int i,j,c;
+    for(i=1;i<=p*q;i++)
     {
         for(j=1;j<=i;j++)
         {
-            if(a*i==b*j)
+            if(p*i==q*j)
                 {
-                    printf("%d ",a*i);
+                    printf("%d ",p*i);
                     c=1;
                 }  
             if(c==1)
