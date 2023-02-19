5f958751c1faa8c6df766b522b2eefbe344cfc58
Atul Kumar Bisoyi <94751610+AtulKumarBisoyi@users.noreply.github.com>
2023-02-19 23:12:39 +0530

    Update d9p8.c

--- ps-programs/d9p8.c
+++ ps-programs/d9p8.c
@@ -1,21 +1,9 @@
-#include<stdio.h>
-void main()
-{
-    int n,s;
-    printf("Enter n = ");
-    scanf("%d",&n);
-    s=n;
-    while(n>0)
-    {
-        if(n-1>0)
-        {
-            s=s*(n-1);
-        }
-        n=n-1;
-    }
-    printf("%d",s);
-    while(1)
-    {
-        
-    }
+int trailingZeroes(int n){
+    for(int i=1;i<=n;i++)
+    {int fact=fact*i;}    
+    int count = 0;
+    for (int i = 5; n / i >= 1; i *= 5)
+        count += n / i;
+ 
+    return count;
 }
